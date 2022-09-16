8826e6c4d0e41ae2f56a9c31546d9dd334d21eb6
Hung Guei <hguei@moporgic.info>
2022-09-16 19:26:50 +0800

    refactor for the new naming standard and improve arguments

--- 2048.py
+++ 2048.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 """
-Framework for 2048 & 2048-like Games (Python 3)
+Framework for 2048 & 2048-Like Games (Python 3)
 2048.py: Main file for the 2048 framework
 
-Author: Hung Guei (moporgic)
+Author: Hung Guei
         Computer Games and Intelligence (CGI) Lab, NYCU, Taiwan
         https://cgilab.nctu.edu.tw/
 """
@@ -12,9 +12,9 @@ Author: Hung Guei (moporgic)
 from board import board
 from action import action
 from episode import episode
-from statistic import statistic
-from agent import player
-from agent import rndenv
+from statistics import statistics
+from agent import random_slider
+from agent import random_placer
 import sys
 
 
@@ -23,58 +23,59 @@ if __name__ == '__main__':
     print()
 
     total, block, limit = 1000, 0, 0
-    play_args, evil_args = "", ""
-    load, save = "", ""
-    summary = False
-    for arg in sys.argv[1:]:
-        if "--total=" in arg:
-            total = int(arg[(arg.index("=") + 1):])
-        elif "--block=" in arg:
-            block = int(arg[(arg.index("=") + 1):])
-        elif "--limit=" in arg:
-            limit = int(arg[(arg.index("=") + 1):])
-        elif "--play=" in arg:
-            play_args = arg[(arg.index("=") + 1):]
-        elif "--evil=" in arg:
-            evil_args = arg[(arg.index("=") + 1):]
-        elif "--load=" in arg:
-            load = arg[(arg.index("=") + 1):]
-        elif "--save=" in arg:
-            save = arg[(arg.index("=") + 1):]
-        elif "--summary" in arg:
-            summary = True
+    slide_args, place_args = "", ""
+    load_path, save_path = "", ""
+    args = sys.argv[1:]
+    while args:
+        arg = args.pop(0).lstrip("-")
+        match_arg = lambda flag: arg.find(flag) == 0
+        next_opt = lambda: arg[(arg.find("=") + 1):] if (arg.find("=") + 1) else args.pop(0)
+        if match_arg("total"):
+            total = int(next_opt())
+        elif match_arg("block"):
+            block = int(next_opt())
+        elif match_arg("limit"):
+            limit = int(next_opt())
+        elif match_arg("slide") or match_arg("play"):
+            slide_args = next_opt()
+        elif match_arg("place") or match_arg("env"):
+            place_args = next_opt()
+        elif match_arg("load"):
+            load_path = next_opt()
+        elif match_arg("save"):
+            save_path = next_opt()
 
-    stat = statistic(total, block, limit)
+    stats = statistics(total, block, limit)
 
-    if load:
-        input = open(load, "r")
-        stat.load(input)
+    if load_path:
+        input = open(load_path, "r")
+        stats.load(input)
         input.close()
-        summary |= stat.is_finished()
+        if stats.is_finished():
+            stats.summary()
 
-    with player(play_args) as play, rndenv(evil_args) as evil:
-        while not stat.is_finished():
-            play.open_episode("~:" + evil.name())
-            evil.open_episode(play.name() + ":~")
+    with random_slider(slide_args) as slide, random_placer(place_args) as place:
+        while not stats.is_finished():
+            # print("======== Game %d ========" % stats.step(), file=sys.stderr)
+            slide.open_episode("~:" + place.name())
+            place.open_episode(slide.name() + ":~")
 
-            stat.open_episode(play.name() + ":" + evil.name())
-            game = stat.back()
+            stats.open_episode(slide.name() + ":" + place.name())
+            game = stats.back()
             while True:
-                who = game.take_turns(play, evil)
+                who = game.take_turns(slide, place)
                 move = who.take_action(game.state())
+                # print("%s\n#%d %s: %s" % (game.state(), game.step(), who.name(), move), file=sys.stderr)
                 if not game.apply_action(move) or who.check_for_win(game.state()):
                     break
-            win = game.last_turns(play, evil)
-            stat.close_episode(win.name())
+            win = game.last_turns(slide, place)
+            stats.close_episode(win.name())
 
-            play.close_episode(win.name())
-            evil.close_episode(win.name())
+            slide.close_episode(win.name())
+            place.close_episode(win.name())
 
-    if summary:
-        stat.summary()
-
-    if save:
-        output = open(save, "w")
-        stat.save(output)
+    if save_path:
+        output = open(save_path, "w")
+        stats.save(output)
         output.close()
 
